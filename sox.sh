212aaca19ed3a3a9c4be3a184b069dfa8c8d34bf
John Brilla <jbrilla17@gmail.com>
2015-09-14 19:42:52 +1000

    Create sox.sh
    starting sox

---
+++ sox.sh
@@ -0,0 +1,8 @@
+#SOX Formulas
+
+#Example - SOX - 23.98 to 24 for DCP
+#sox left.wav sound/left.wav speed 1.001
+
+
+#LOOP - enter folder and run on wav files.
+for name in *.wav; do sox "$name"  "${name%.*}_24.wav" speed 1.001; done
